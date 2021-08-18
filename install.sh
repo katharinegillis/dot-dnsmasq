10695850ddad88e94987bfd386e443d5c47fa47b
Katharine Cordes <gillis.katharine@gmail.com>
2021-08-18 11:47:01 -0600

    fix: fix dns resolver issues on ubuntu 20

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+PKG_PATH=$2
 SYSTEM=$3
 
 if [ ! -d "$HOME/utils" ]; then
@@ -10,16 +11,25 @@ cd "$HOME/utils" || return
 
 git clone git@github.com:katharinegillis/docker-dnsmasq.git dnsmasq
 
-echo "# .docker local urls" >> $HOME/utils/dnsmasq/dnsmasq.conf
-echo "address=/.docker/127.0.0.1" >> $HOME/utils/dnsmasq/dnsmasq.conf
+if ! grep -Fxq "# .docker local urls" "$HOME/utils/dnsmasq/dnsmasq.conf"; then
+    echo "# .docker local urls" >> $HOME/utils/dnsmasq/dnsmasq.conf
+    echo "address=/.docker/127.0.0.1" >> $HOME/utils/dnsmasq/dnsmasq.conf
+fi
 
 if [ "$SYSTEM" == "linux" ]; then
-    sudo systemctl disable systemd-resolved
-    sudo systemctl stop systemd-resolved
+    if [ ! -d /etc/systemd/resolved.conf.d ]; then
+        sudo mkdir /etc/systemd/resolved.conf.d
+    fi
+    sudo cp "$PKG_PATH/config/noresolved.conf" /etc/systemd/resolved.conf.d/noresolved.conf
+    sudo systemctl restart systemd-resolved
+
     sudo rm /etc/resolv.conf
 
     echo "nameserver 127.0.0.1" | sudo tee /etc/resolv.conf
-    echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+    echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+
+    sudo cp "$PKG_PATH/config/disableresolv.conf" /etc/NetworkManager/conf.d/disableresolv.conf
+    sudo systemctl restart NetworkManager
 else
     echo "\e[33mPlease ensure your DNS resolver is pointed to 127.0.0.1 as preferred.\e[0m"
 fi
